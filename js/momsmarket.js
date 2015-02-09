9a8ec6e23d32ec9b44de8c972e6394698c757567
Andrew Pawluczkowycz <awpawluczkowycz@gmail.com>
2015-02-09 13:04:39 -0500

    upload new moms market site.

---
+++ js/momsmarket.js
@@ -0,0 +1,79 @@
+
+	$(document).ready(function() {
+	
+		$("i.fa-bars").toggle(function() {
+			$("nav").fadeIn("slow")},
+			function() {
+				$("nav").fadeOut("slow")});
+				
+		$(window).on('resize', (function(){
+			var navDisplay = $("nav").css("display");
+			var windowWidth = $(window).width();
+			var bars = $("i.fa-bars").css("display");
+			if(windowWidth > 768 || bars == "none") {
+			$("nav").slideDown();}
+			}));
+	});
+
+		$(document).ready(function() {
+		
+		    var target;
+			var triggers;
+			var images;
+			var lastElem;
+			var mask;
+			var imgWidth
+			
+			function calculateWidth() {
+				target;
+				triggers = $('ul.triggers li');
+				images = $('ul.images li');
+				lastElem = triggers.length-1;
+				mask = $('.mask ul.images');
+			
+			
+				imgWidth = images.width();
+				images.css('width', imgWidth);
+		
+				triggers.first().addClass('active');
+				mask.css('width', imgWidth*(lastElem+1) +'px');
+				slideshow();
+			}
+			
+			function slideshow() {
+				function sliderResponse(target) {
+					mask.stop(true,false).animate({'left':'-'+ imgWidth*target + 'px'},300);
+					triggers.removeClass('active').eq(target).addClass('active');
+				};
+			
+				triggers.click(function() {
+					if(!$(this).hasClass('active')) {
+						target = $(this).index();
+						sliderResponse(target);
+						resetTiming();
+					};
+				});
+			
+				function sliderTiming() {
+					target = $('ul.triggers li.active').index();
+					target === lastElem ? target = 0 : target = target + 1;
+					sliderResponse(target);
+				};
+			
+				var timingRun = setInterval(function() { sliderTiming(); }, 5000);
+				function resetTiming() {
+					clearInterval(timingRun);
+					timingRun = setInterval(function() { sliderTiming(); }, 5000);
+				};
+			}
+			
+			calculateWidth();
+			$(window).resize(function() {
+			
+				var newWidth = $('#maincontentcontainer').width();
+				mask.css('width', newWidth*(3) +'px');
+				images.css('width', newWidth);
+				imgWidth = newWidth;
+				$(window).mouseup(slideshow);
+			});
+		});
